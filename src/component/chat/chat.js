13a68c9f0908ba3934bfb654533fa12a609eab64
wang <13269403044@163.com>
2018-06-01 16:50:10 +0800

    unread filter

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -3,6 +3,7 @@ import io from 'socket.io-client'
 import { List, InputItem, NavBar, Icon } from 'antd-mobile'
 import { connect } from 'react-redux'
 import { getMsgList, recvMsg, sendMsg } from './../../redux/chat.redux'
+import { getChatId } from './../../util'
 // eslint-disable-next-line
 const socket = io('ws://localhost:9093')
 
@@ -48,6 +49,8 @@ class Chat extends React.Component {
     if (!users[userid]) {
       return null
     }
+    const chatid = getChatId(userid, this.props.user._id)
+    const chatmsgs = this.props.chat.chatmsg.filter(v => v.chatid === chatid)
     return (
       <div id="chat-page">
         <NavBar 
@@ -58,7 +61,7 @@ class Chat extends React.Component {
           }}>
           { users[userid].name }
         </NavBar>
-        {this.props.chat.chatmsg.map((v) => {
+        {chatmsgs.map((v) => {
           const avatar = require(`../img/${users[v.from].avatar}.png`)
           return v.from === userid ? (
             <List key={v._id}>
