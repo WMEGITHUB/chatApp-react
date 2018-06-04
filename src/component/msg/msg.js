ee75c74f2be8d4630d4efa7e018de377f85433f5
wang <13269403044@163.com>
2018-06-04 13:41:16 +0800

    msglist

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,22 +1,48 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { List } from 'antd-mobile'
 
 @connect(
   state => state
 )
 class Msg extends React.Component {
+  getLast(arr) {
+    return arr[arr.length - 1]
+  }
   render() {
-    console.log(this.props)
+    const Item = List.Item
+    const Brief = Item.Brief
+
+    const userid = this.props.user._id
+    const userinfo = this.props.chat.users
+
     const msgGroup = {}
     this.props.chat.chatmsg.forEach(v => {
       msgGroup[v.chatid] = msgGroup[v.chatid] || []
       msgGroup[v.chatid].push(v)
     });
-    console.log(msgGroup)
+    
+    const chatList = Object.values(msgGroup)
     // 按照聊天用户分组  根据chatid
     return (
       <div>
-        msg
+          {chatList.map(v => {
+            const lastItem = this.getLast(v)
+            const targetId = v[0].from === userid ? v[0].to : v[0].from
+            if (!userinfo[targetId]) {
+              return null
+            }
+            return (
+              <List
+                key={v._id}>
+                <Item 
+                  thumb={require(`../img/${userinfo[targetId].avatar}.png`)}>
+                  {lastItem.content}
+                <Brief>{userinfo[targetId].name}</Brief>
+                </Item>
+              </List>
+            )
+          })}
       </div>
     )
   }
